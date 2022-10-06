a4bf9255d19a5c9a6945ff870e328deaeae56b03
amnads <amnads@imt-soft.com>
2022-10-06 17:38:51 +0700

    laravel 8

---
+++ FreshHomeStay/resources/views/facilities.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.index')
+@section('title','facilities')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fresh HomeStay - Facilities</title>
+    <style>
+        .pop:hover{
+            border-top-color: var(--teal) !important;
+            transform: scale(1.03);
+            transition: all 0.3s;
+        }
+    </style>
+</head>
+
+<body class="bg-light">
+
+    <!-- navbar -->
+    @include('layouts.header')
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Iure dicta molestiae repudiandae <br> magnam quidem qui
+            assumenda eos fuga commodi eligendi.
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class ="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi2.png" width="40px">
+                        <h5 class ="m-0 ms-3">Wifi</h5>
+                        </div>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Libero itaque totam animi velit possimus quidem.
+                        </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class ="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi2.png" width="40px">
+                        <h5 class ="m-0 ms-3">Wifi</h5>
+                        </div>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Libero itaque totam animi velit possimus quidem.
+                        </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class ="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi2.png" width="40px">
+                        <h5 class ="m-0 ms-3">Wifi</h5>
+                        </div>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Libero itaque totam animi velit possimus quidem.
+                        </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class ="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi2.png" width="40px">
+                        <h5 class ="m-0 ms-3">Wifi</h5>
+                        </div>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Libero itaque totam animi velit possimus quidem.
+                        </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class ="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi2.png" width="40px">
+                        <h5 class ="m-0 ms-3">Wifi</h5>
+                        </div>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Libero itaque totam animi velit possimus quidem.
+                        </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class ="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi2.png" width="40px">
+                        <h5 class ="m-0 ms-3">Wifi</h5>
+                        </div>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Libero itaque totam animi velit possimus quidem.
+                        </p>
+                </div>
+            </div>
+  
+           
+        </div>
+    </div>
+    <!-- footer -->
+    @include('layouts.footer')
+    <!-- Initialize Swiper -->
+
+
+
+
+</body>
+
+</html>
---
+++ FreshHomeStay/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,60 @@
+ <!-- Carousel -->
+    <div class="container-fluid px-lg-4 mt-4 ">
+        <!-- Swiper -->
+        <div class="swiper swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/1.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/2.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/3.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/4.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/5.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/6.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/7.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/8.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/9.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/10.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/11.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/12.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="resources/images/carousel/13.jpg" class="w-100 d-block" height="350px;" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Initialize Swiper -->
+    <script>
+        var swiper = new Swiper(".swiper-container", {
+            spaceBetween: 30,
+            effect: "fade",
+            loop: true,
+            autoplay: {
+                delay: 3500,
+                disableOnInteraction: false,
+            }
+        });
+    </script>
